c72ff40ebb16952782af1c9dba08f7a135c9b42e
XSR700 <89107047+XSR700@users.noreply.github.com>
2021-11-06 11:49:56 -0500

    Add files via upload

---
+++ Vacation_Search/config.py
@@ -0,0 +1,11 @@
+weather_api_key="2c22189d3f4e0d6855547470e5ba9481"
+
+g_key = "AIzaSyC1JPlLEoVdPM20fwsHw8A5JRx6YirvMbw"
+
+
+#conda activate pythondata
+
+#$ conda install -c conda-forge gmaps
+
+#jupyter nbextension enable --py gmaps
+#jupyter nbextension enable --py widgetsnbextension
